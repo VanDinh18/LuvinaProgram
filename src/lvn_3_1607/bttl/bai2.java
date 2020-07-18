4d958539d55aea6ae90c0b5a9a6d4d59495fe0a5
VanDinh18 <nguyendinh180299@gmail.com>
2020-07-18 21:35:58 +0700

    update 16/07

--- src/lvn_3_1607/bttl/bai2.java
+++ src/lvn_3_1607/bttl/bai2.java
@@ -2,32 +2,26 @@ package lvn_3_1607.bttl;
 //tim day dai nhat
 public class bai2 {
     public static void main(String[] args) {
-        int[] A = {9, 2, 5, 8, 3, 10, 14, 17, 7, 20, 22, 25, 40, 45};
-        int i = 1;
+        int[] A = {9, 2, 5, 8, 3, 10, 14, 17, 7, 20}; //n=10
+        int i = A.length-1;
         int max = 1;
-        int d=0;
-        int c=0;
-        int vtd = 0;
-        int vtc = 0;
-        while (i < A.length){
+        int d=A.length-1;
+        int c=A.length-1;
+        int vtd = A.length-1;
+        int vtc = A.length-1;
+        while (i >= 1){
             if(A[i] < A[i-1]){
-                if(i-d > max){
-                    max = i - d;
-                    vtd = d;
-                    vtc = i - 1;
+                if(c - i > max){
+                    max = vtc - i;
+                    vtd = i;
+                    vtc = c;
                 }
                 d=i;
                 c=i;
             }
-            i++;
+            i--;
         }
-        if(i-d > max){
-            max = i - d;
-            vtd = d;
-            vtc = i - 1;
-        }
-        d=i;
-        c=i;
+
         System.out.println(max);
         System.out.println(vtd + "-" + vtc);
     }
