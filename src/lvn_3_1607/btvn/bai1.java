2489d1cf0c4ef3184b8f3128893c74acc18defa9
VanDinh18 <nguyendinh180299@gmail.com>
2020-07-19 22:06:30 +0700

    update btvn16/07

---
+++ src/lvn_3_1607/btvn/bai1.java
@@ -0,0 +1,58 @@
+package lvn_3_1607.btvn;
+import java.util.Arrays;
+
+public class bai1 {
+    public static void sapxep1(int[] A){
+        int n = A.length;
+        int temp = 0;
+        int min;
+        for(int i=0; i<n-1; i++){
+            min = i;
+            for(int j=i+1; j<n; j++){
+                if(A[min] > A[j]){
+                    min = j;
+                }
+            }
+            temp = A[min];
+            A[min] = A[i];
+            A[i] = temp;
+        }
+        System.out.println(Arrays.toString(A));
+    }
+    public static void sapxep2(int[] A){
+        int n = A.length;
+        int temp = 0;
+        for(int i=0; i<n; i++){
+            for(int j=n-1; j>i; j--){
+                if(A[j] < A[j-1]){
+                    temp = A[j];
+                    A[j] = A[j-1];
+                    A[j-1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(A));
+    }
+
+    public static void sapxep3(int[] A){
+        int n = A.length;
+        int temp;
+        int j;
+        for(int i=1; i<n; i++){
+            temp = A[i];
+            j = i-1;
+            while (j >= 0 && temp < A[j]){
+                A[j+1] = A[j];
+                A[j] = temp;
+                j--;
+            }
+        }
+        System.out.println(Arrays.toString(A));
+    }
+    public static void main(String[] args) {
+        int[] A = {17, 21, 9, 12, 6, 18, 4, 20, 7, 14};
+        sapxep1(A);
+        sapxep2(A);
+        sapxep3(A);
+    }
+}
